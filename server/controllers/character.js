0ebe495016dcfc570b109850608c2e9142e6ab31
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-04-19 13:47:54 -0700

    added character model to the back end

---
+++ server/controllers/character.js
@@ -0,0 +1,2 @@
+import Character from "../models/Post.js";
+import User from "../models/User.js";
---
+++ server/models/Character.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const characterSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    level: {
+        type: Number,
+        required: true,
+        trim: true,
+        default: 1,
+    },
+    race: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    charClass: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    alignment: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    experience: {
+        type: Number,
+        required: true,
+        default: 0,
+        trim: true,
+    },
+    strength: {
+        type: Number,
+        required: true,
+    },
+    dexterity: {
+        type: Number,
+        required: true,
+    },
+    constitution: {
+        type: Number,
+        required: true,
+    },
+    intelligence: {
+        type: Number,
+        required: true,
+    },
+    wisdom: {
+        type: Number,
+        required: true,
+    },
+    charisma: {
+        type: Number,
+        required: true,
+    },
+});
+
+const Character = mongoose.model("Character", characterSchema);
+
+export default Character;
