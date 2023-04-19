0b658c45a2f88c51beb0e6c49ecfb0fa9fb71cd3
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-04-19 13:14:41 -0700

    added slices of state

---
+++ client/src/state/index.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    mode: "light",
+    user: null,
+    token: null,
+    posts: [],
+};
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        },
+        setFriends: (state, action) => {
+            if (state.user) {
+                state.user.friends = action.payload.friends;
+            } else {
+                console.error("user friends non-existent :(");
+            }
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        },
+        setPost: (state, action) => {
+            const updatedPosts = state.posts.map((post) => {
+                if (post._id === action.payload.post._id) return action.payload.post;
+                return post;
+            });
+            state.posts = updatedPosts;
+        },
+        // setCharacters: (state, action) => {
+        //     state.characters = action.payload.characters;
+        // },
+        // setCharacter: (state, action) => {
+        //     const updatedCharacters = state.characters.map((character) => {
+        //         if (character._id === action.payload.character._id) return action.payload.character;
+        //         return character;
+        //     });
+        //     state.characters = updatedCharacters;
+        // },
+    },
+});
