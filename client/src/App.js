7781dac7f521856f9b4898b1dcf42d91693df0d8
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-04-26 12:46:39 -0700

    added all imports on the navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -14,19 +14,20 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route
-            path="/home"
-            element={<HomePage />}
-          />
-          <Route
-            path="/profile/:userId"
-            element={<ProfilePage />}
-          />
-        </Routes>
-
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={<HomePage />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={<ProfilePage />}
+            />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   )
